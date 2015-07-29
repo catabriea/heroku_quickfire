0442425d15306120fe54a80d8dba65fe38460477
Catabriea Muellerleile <cmuellerleile@Catabrieas-MacBook-Air.local>
2015-07-29 11:23:15 -0500

    Added application

---
+++ server/public/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(dir__name, "./public/views/index.html"));
+});
+
+app.listen(app.get('port'), funtion(){
+    console.log("Hey! We are listening on Port: " + app.get('port'));
+});
