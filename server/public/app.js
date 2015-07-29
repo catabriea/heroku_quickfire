2d114440ac7d1a7ca8ab85ce51a39424a26eb786
Catabriea Muellerleile <cmuellerleile@Catabrieas-MacBook-Air.local>
2015-07-29 11:33:38 -0500

    Forgot dir name underscore

--- server/public/app.js
+++ server/public/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port',(process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(dir__name, "./public/views/index.html"));
+    res.sendFile(path.join(__dirname, "./public/views/index.html"));
 });
 
 app.listen(app.get('port'), funtion(){
